494abd5b9f8d56c0affbe46f27f30c82d6e62fcb
tulsiid <tulsideval08@gmail.com>
2026-07-22 03:53:46 +0530

    Time: 26 ms (26.53%), Space: 44.3 MB (32.91%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        if(s1.length()>s2.length()) return false;
+        HashMap<Character,Integer> map=new HashMap<>();
+        for(char c:s1.toCharArray())
+        {
+            map.put(c,map.getOrDefault(c,0)+1);
+        }
+        int l=0;
+        int count=s1.length();
+        for(int r=0;r<s2.length();r++)
+        {
+            char ch=s2.charAt(r);
+            int val=map.getOrDefault(ch,0);
+            if(val>0) count--;
+            map.put(ch,val-1);
+            if(count==0) return true;
+            if(r-l+1==s1.length())
+            {
+                char leftchar=s2.charAt(l);
+                int leftval=map.get(leftchar);
+                if(leftval>=0) count++;
+                map.put(leftchar,leftval+1);
+                l++;
+             
+            }
+        }
+        return false;
+        
+    }
+}
